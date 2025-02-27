59ef7f994aedb33155d5e05a3b6cf9d7dc50530a
22CS102 <22CS102@PSRENGG.AC.IN>
2025-02-27 10:42:40 +0530

    first commit

---
+++ Backend/routes/receive.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const Receiver = require('../Models/receiveModel');
+router.post('/receiver', async (req, res) => {
+    try {
+        const { name, phoneNumber, address, location } = req.body;
+        if (!name || !phoneNumber || !address || !location) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const newReceiver = new Receiver({
+            name,
+            phoneNumber,
+            address,
+            location
+        });
+        await newReceiver.save();
+        res.status(201).json({ message: 'Receiver details added successfully!' });
+    } catch (error) {
+        console.error('Error saving receiver details:', error);
+        res.status(500).json({ message: 'Server error. Please try again later.' });
+    }
+});
+module.exports = router;
